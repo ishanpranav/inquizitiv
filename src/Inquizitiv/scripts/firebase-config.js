89f41541ea9d61aea16353deff848d07411ae40a
Ishan Pranav <ishan.pranav@outlook.com>
2023-03-22 11:05:10 -0400

    Fix it again

--- src/Inquizitiv/scripts/firebase-config.js
+++ src/Inquizitiv/scripts/firebase-config.js
@@ -4,7 +4,7 @@ import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
     apiKey: "AIzaSyDAEot7Sj1sP7fZh8NelsKSvGYakQ1jyrc",
-    authDomain: "ishanpranav.github.io",
+    authDomain: "inquizitiv-2ca1f.firebaseapp.com",
     projectId: "inquizitiv-2ca1f",
     storageBucket: "inquizitiv-2ca1f.appspot.com",
     messagingSenderId: "53634781396",
