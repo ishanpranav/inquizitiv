6e8d86fe9e5d97f411b6ebf8ff0e0c68e4d2e236
Ishan Pranav <ishan.pranav@outlook.com>
2023-03-22 19:50:56 -0400

    Add angular

---
+++ src/scripts/AppComponent.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app-root",
+    templateUrl: "../pages/app-component.html",
+    styleUrls: ["../styles/app-component.css"]
+})
+export class AppComponent {
+    title = 'inquizitiv';
+}
